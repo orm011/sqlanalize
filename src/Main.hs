acd45291e42d2f42f2ef4d7481360f6f56d3fa8e
Oscar Moll <orm@csai.mit.edu>
2016-04-19 00:47:46 -0400

    show sql as well as pretty printed ast

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,11 @@
 import System.Environment
 import Language.SQL.SimpleSQL.Parser
 import Language.SQL.SimpleSQL.Syntax
+import Language.SQL.SimpleSQL.Pretty
 import Data.List
 import Data.Maybe
 import Data.Either
+import Text.Groom
 
 main :: IO ()
 main = do a <- getArgs
@@ -13,19 +15,22 @@ main = do a <- getArgs
                             num_errors = length $ fst $ parsed
                             good_ones = snd $ parsed
                             idens = map get_all_idens good_ones
-                            in sequence_ $ (map (putStrLn . show) idens) ++ [putStrLn $ show num_errors]
+                            in sequence_ $ (map (\[x,y,z] -> putStrLn x >> putStrLn "" >> putStrLn y >> putStrLn "" >> putStrLn z >> putStrLn "---------")  (transpose [map prettyQueryExpr good_ones, map show idens, map groom good_ones])) ++ [putStrLn $ show num_errors]
             _ -> error "we need an input file name"
 
+extract_name (Name x) = x
 {- finds the identifiers used within an expression, possibly deep within -}
 get_idens :: ValueExpr -> [String]
-get_idens (Iden ls) = [intercalate "." (map show ls)]
+get_idens (Iden ls) = [intercalate "." (map extract_name ls)]
 get_idens Star = ["star"] {- fix later by using schema -}
 get_idens (App _ vals) = concat $ map get_idens vals
 get_idens (AggregateApp { aggArgs, aggFilter }) = concat $ (map get_idens aggArgs) ++ (map get_idens (maybeToList aggFilter))
+get_idens (BinOp _1  _  _2) = get_idens _1 ++ get_idens _2
+get_idens (Parens x) = get_idens x
 get_idens _ = []
 
 get_all_idens :: QueryExpr -> [String]
-get_all_idens Select { qeSelectList } = concat $ map (get_idens . fst) qeSelectList
+get_all_idens Select { qeSelectList, qeWhere  } = concat $ (map (get_idens . fst) qeSelectList) ++ (map get_idens (maybeToList qeWhere))
 get_all_idens  _ = []
 
 
