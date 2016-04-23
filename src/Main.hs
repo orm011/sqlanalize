488d4789f58b8c26b47eebc1667fabb3f85413b5
Oscar Moll <orm@csai.mit.edu>
2016-04-22 23:11:31 -0400

    do stats

--- src/Main.hs
+++ src/Main.hs
@@ -19,24 +19,48 @@ import System.IO
 main :: IO ()
 main = do a <- getArgs
           case a of
-            [str] -> do handle <- openFile str ReadMode
-                        mainloop str handle 0 0
-                        hClose handle
+            [filename] -> do handle <- openFile filename ReadMode
+                             mainloop filename handle initial_stats
+                             hClose handle
             _ -> error "we need an input file name"
 
-mainloop :: String -> Handle -> Int -> Int -> IO ()
-mainloop filename handle lineno errno  =
+data Stats = Stats
+  { stats_total::Int
+  , stats_parsed::Int
+  , stats_simple::Int
+  , stats_column_histogram::Map.Map Int Int
+  } deriving Show
+
+initial_stats = Stats
+  { stats_total = 0
+  , stats_parsed = 0
+  , stats_simple = 0
+  , stats_column_histogram = Map.empty
+  }
+
+incr hist cols =  Map.insertWith (+) cols 1 hist
+
+mainloop :: String -> Handle -> Stats -> IO ()
+mainloop filename handle stats@(Stats { stats_total, stats_parsed, stats_simple, stats_column_histogram })  =
   hIsEOF handle >>=
   (\iseof -> if iseof
-         then putStrLn ("parse errors / total queries  = " ++ show errno  ++ " / " ++ show (lineno + 1) )
+         then putStrLn (show stats) >> putStrLn ("parse errors / total queries  = " ++ show (stats_total - stats_parsed)  ++ " / " ++ show stats_total )
          else  (hGetLine handle
                 >>= (\query ->
-                      let (delta_err, o)  = prettyParseErr filename lineno query
+                      let (msg, newstats) =
+                            let stats_updated_count =  stats { stats_total = stats_total + 1}
+                            in case parseMySQLQuery filename (Just (stats_total + 1, 0)) query of
+                              Left ParseError { peFormattedError } -> (peFormattedError, stats_updated_count )
+                              Right _1 -> let stats_updated_parsed = stats_updated_count { stats_parsed = stats_parsed+1 }
+                                          in if not (is_simple_scan _1) then (groom _1, stats_updated_parsed )
+                                             else let cols =  distinct_columns_accessed $ get_all_idens _1
+                                                  in (groom _1, stats_updated_parsed { stats_simple = stats_simple + 1
+                                                                                     , stats_column_histogram = incr stats_column_histogram cols } )
                       in (putStrLn (show query)
                           >> putStrLn ""
-                          >> putStrLn o
+                          >> putStrLn msg
                           >> putStrLn "---------"
-                          >> mainloop filename handle (lineno+1) (errno+delta_err))
+                          >> mainloop filename handle newstats)
                     )
                )
   )
@@ -88,6 +112,10 @@ get_all_idens Select { qeSelectList,  qeWhere  } =
   ColumnsUsed { select_clause = group_by_sum $ concat $ (map (get_idens . fst) qeSelectList)
               , where_clause = group_by_sum $ concat $ (map get_idens (maybeToList qeWhere)) }
 
+distinct_columns_accessed :: ColumnsUsed -> Int
+distinct_columns_accessed ColumnsUsed {select_clause, where_clause} =
+  Map.fromList (select_clause ++ where_clause) |> Map.toList |> length
+
 {- select (agg) from single_table where -}
 is_simple_scan :: QueryExpr -> Bool
 is_simple_scan Select {
@@ -102,7 +130,10 @@ is_simple_scan Select {
 is_simple_scan _ = False
 
 {-
-is_simple_scan
-is_join
-is_aggregate
+
+note:
+need to check for names that are nested within constructs not covered at the moment...
+simple queries right now include queries like this one:
+is_simple_scan  $ justParse "select count(distinct foo) from bar"
+
 -}
